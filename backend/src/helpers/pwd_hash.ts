b54b3e0b31492d82645dbf1e112897be37942aa7
adhigop13 <adhigop13@gmail.com>
2026-06-03 00:38:50 +0800

    Reduce number of hash rounds to 12 for bcrypt

--- backend/src/helpers/pwd_hash.ts
+++ backend/src/helpers/pwd_hash.ts
@@ -2,7 +2,7 @@ export async function hashPwd(password: string) {
     // Dynamic import loaded only when the function is called
     const { genSalt, hash } = await import("bcrypt-ts");
 
-    const saltRounds = 15;
+    const saltRounds = 12;
     const salt = await genSalt(saltRounds);
     const result = await hash(password, salt);
     return result;  //hashed pwd
