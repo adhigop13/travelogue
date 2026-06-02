d7bf02c7d05e3678c82378aa1de063f1bf4ba23f
adhigop13 <adhigop13@gmail.com>
2026-06-03 00:41:00 +0800

    Add finished Home & Login pages Create new AWS CDN and link to CDN for video/img assets Remove unused App.css

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,60 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
     const navigate = useNavigate();
+    const [showContent, setShowContent] = useState(false);
+    const assetUrl = import.meta.env.VITE_ASSET_URL;
+    const handleLoginClick = () => {
+        navigate('/login');
+    }
     return (
-        <>
 
-            <p>This is homepage</p>
-            <button onClick = {() => navigate('/login')}>Go to Login page</button>
-        </>
+        <div className="relative min-h-screen w-full flex flex-col">
+            {/* Background video */}
+            <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="absolute inset-0 w-full h-full object-cover z-0"
+                >
+                <source src={`${assetUrl}/airplane-flying.mp4`} type="video/mp4" />
+            </video>
+            
+        
+            {/* Dark overlay */}
+            <div className="absolute inset-0 bg-black/40 z-10"></div>
+
+            {/* navBar with login & register buttons and Travelogue site name */}
+            <div className="flex justify-between w-full bg-black z-20">
+                <div className = 'font-sans text-2xl font-bold px-4 py-3'>
+                    travelogue
+                </div>
+                <div className= "flex gap-4 px-9">
+                    <button onClick = {handleLoginClick}>
+                        Login
+                    </button>
+                </div>
+            </div>
+
+            {/* Now we want to add the self-typing "Let's plan a wonderful trip!" */}
+            <div className="flex-1 flex flex-col justify-center items-center text-center z-20">
+                <h1 className="text-4xl font-cursive tracking-normal leading-relaxed antialiased animate-sweep p-5"
+                    onAnimationEnd={() => setShowContent(true)}>
+                        Let's plan a wonderful trip together!
+                </h1>
+                {/* Add the register button */}
+                <div
+                    className={`mt-6 h-10 transition-all duration-500 ease-out
+                        ${showContent ? "opacity-100 translate-y-0" : "opacity-0 translate-y-2"}
+                        `}
+                >
+                    <button className="bg-blue-600 text-white px-6 py-2 rounded-lg">
+                        Get Started
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,6 @@
 import axios from "axios"
 import { useState } from "react"
-import { Navigate, useNavigate } from "react-router-dom";
-import backgroundVideo from "../assets/bg2.mp4"
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 
 
@@ -9,6 +8,7 @@ export default function Login() {
     const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const assetUrl = import.meta.env.VITE_ASSET_URL;
     const notifySuccess = () => toast("Login succeeded!");
     const notifyFailure = () => toast("Login failed");
     async function handleLogin() {
@@ -25,25 +25,33 @@ export default function Login() {
     }
 
     return (
-    <div className="relative min-h-screen flex items-center justify-center">
-           {/* Background video */}
+    <div className="relative min-h-screen flex flex-col items-center justify-center">
+        {/* Background video */}
         <video
             autoPlay
             loop
             muted
             playsInline
-            className="absolute inset-0 w-full h-full object-cover"
+            className="absolute inset-0 w-full h-full object-cover z-0"
             >
-            <source src={backgroundVideo} type="video/mp4" />
+            <source src={`${assetUrl}/bg2.mp4`} type="video/mp4" />
         </video>
 
-         {/* Dark overlay */}
-        <div className="absolute inset-0 bg-black/40"></div>
+        {/* Dark overlay */}
+        <div className="absolute inset-0 bg-black/40 z-10"></div>
 
+        {/* Travelogue logo */}
+        <div className="p-8 w-full flex justify-center z-20">
+            <Link to = "/" >
+                <h1 className="font-sans text-2xl font-bold text-center mb-6 bg-transparent border-none">
+                    travelogue
+                </h1>
+            </Link>
+        </div>
         {/* Login card */}
-        <div className="relative backdrop-blur-sm p-8 rounded-2xl shadow-lg w-96 border">
+        <div className="relative backdrop-blur-sm p-8 rounded-2xl shadow-lg w-96 border z-20">
             <h1 className="text-white text-3xl font-bold text-center mb-6">
-                Welcome Back
+                Welcome Back!
             </h1>
             <div className="space-y-4">
                 <label className="block">
@@ -66,11 +74,12 @@ export default function Login() {
                 </label>
                 <div className = 'text-center'>
                     <button onClick={handleLogin} className="w-full mt-1 p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">   
-                        Submit
+                        Log in
                     </button>
                 </div>
             </div>
         </div>
         <ToastContainer />
-    </div>)
+    </div>
+    )
 }
